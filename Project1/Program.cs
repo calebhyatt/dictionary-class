66923268825d101c86477a0a3c5b91b32ae8e879
calebhyatt <calebphyatt@gmail.com>
2023-11-19 19:47:00 -0500

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,67 @@
+﻿// CSC 204 - Algorithms
+// Project 1, Caleb Hyatt
+
+using Project1;
+
+var myDict = new Webster();
+
+/* Scenario 0b */
+myDict.Add("box");
+myDict.Add("book");
+myDict.Add("build");
+myDict.Add("car");
+myDict.Add("can");
+myDict.Add("CSC204");
+
+/* Scenario 1 */
+var s1 = myDict.GetWordsForPrefix("bo");
+Console.WriteLine($"Scenario 1 // GetWordsForPrefix(\"bo\")\n\n{string.Join("\n", s1)}");
+
+/* Scenario 2 */
+var s2 = myDict.GetWordsForPrefix("ca");
+Console.WriteLine($"\nScenario 2 // GetWordsForPrefix(\"ca\")\n\n{string.Join("\n", s2)}");
+
+/* Scenario 3 & 4 */
+myDict.Delete("box");
+// myDict.Delete("boom"); // this will throw an error in the project; comment out to continue
+Console.WriteLine("\nScenario 3 & 4 // Delete(\"box\") and Delete(\"boom\")");
+
+/* Scenario 5 */
+var s5 = myDict.GetAllWords();
+Console.WriteLine($"\nScenario 5 // GetAllWords()\n\n{string.Join("\n", s5)}");
+
+/* Scenario 6 */
+// myDict.Add("book"); // this will throw an error in the project; comment out to continue
+Console.WriteLine("\nScenario 6 // Add(\"book\")");
+
+/* Scenario 7 */
+var s7 = myDict.Size();
+Console.WriteLine($"\nScenario 7 // Size\n{s7}");
+
+/* Scenario 8 - in separate file */
+
+/* Scenario 9 */
+myDict.SetState(Webster.States.ReadOnly);
+Console.WriteLine("\nScenario 9 // SetState(Webster.States.ReadOnly)");
+
+/* Secnario 10 */
+myDict.Add("this is a test");
+Console.WriteLine("\nScenario 10 // Add(\"this is a test\")");
+Console.WriteLine("This does not throw an error, rather it will return nothing.");
+
+/* Scenario 11 */
+myDict.SetState(Webster.States.Disabled);
+Console.WriteLine("\nScenario 11 // SetState(Webster.States.Disabled)");
+
+/* Scenario 12 */
+var s12 = myDict.GetWordsForPrefix("b");
+Console.WriteLine("\nScenario 12 // GetWordsForPrefix(\"b\")");
+Console.WriteLine(s12.Count.ToString());
+
+/* Scenario 13 */
+myDict.SetState(Webster.States.ReadWrite);
+Console.WriteLine("\nScenario 13 // SetState(Webster.States.ReadWrite)");
+
+/* Scenario 14 */
+myDict.DeleteAllWords(true);
+Console.WriteLine("\nScenario 14 // DeleteAllWords(true)");
---
+++ Project1/Webster.cs
@@ -0,0 +1,211 @@
+﻿namespace Project1
+{
+    internal class Webster
+    {
+        /* Properties */
+
+        public enum States { ReadWrite, ReadOnly, Disabled };
+        private List<string> Words { get; }
+        private States State { get; set; } = States.ReadWrite;
+
+        /* Initialization */
+
+        // Initialize empty object
+        public Webster()
+        {
+            this.Words = new List<string>();
+        }
+
+        // Initialize object with one word
+        public Webster(string word)
+        {
+            this.Words = new List<string>() { word };
+        }
+
+        // Initialize object with array of words
+        public Webster(string[] words)
+        {
+            var temp = new List<string>();
+
+            foreach (string word in words)
+            {
+                temp.Add(word);
+            }
+
+            this.Words = temp;
+        }
+
+        // Initialize object with list of words
+        public Webster(List<string> words)
+        {
+            this.Words = words;
+        }
+
+        /* Methods */
+
+        public List<string> GetWordsForPrefix(string prefix)
+        {
+            if (this.State == States.Disabled)
+            {
+                return new List<string>();
+            }
+
+            var temp = this.Words.Where(w => w.StartsWith(prefix)).ToList();
+
+            return temp;
+        }
+
+        public List<string> GetAllWords()
+        {
+            if (this.State == States.Disabled)
+            {
+                return new List<string>();
+            }
+
+            return this.Words;
+        }
+
+        // Add one word
+        public void Add(string word)
+        {
+            if (this.State == States.ReadWrite)
+            {
+                if (this.Words.IndexOf(word) > -1)
+                {
+                    throw new Exception($"{word} is already in this instance of Webster.");
+                }
+
+                this.Words.Add(word);
+                this.Words.Sort();
+            }
+        }
+
+        // Add array of words
+        public void Add(string[] words)
+        {
+            if (this.State == States.ReadWrite)
+            {
+                foreach (string word in words)
+                {
+                    if (this.Words.IndexOf(word) > -1)
+                    {
+                        throw new Exception($"{word} is already in this instance of Webster.");
+                    }
+                }
+
+                this.Words.AddRange(words);
+                this.Words.Sort();
+            }
+        }
+
+        // Add list of words
+        public void Add(List<string> words)
+        {
+            foreach (string word in words)
+            {
+                if (this.Words.IndexOf(word) > -1)
+                {
+                    throw new Exception($"{word} is already in this instance of Webster.");
+                }
+            }
+
+            if (this.State == States.ReadWrite)
+            {
+                this.Words.AddRange(words);
+                this.Words.Sort();
+            }
+        }
+
+        // Delete one word
+        public void Delete(string word)
+        {
+            if (this.State == States.ReadWrite)
+            {
+                var index = this.Words.IndexOf(word);
+
+                if (index > -1)
+                {
+                    this.Words.RemoveAt(index);
+                    this.Words.Sort();
+                }
+                else
+                {
+                    throw new Exception($"{word} was not found in this instance of Webster.");
+                }
+            }
+        }
+
+        // Delete array of words
+        public void Delete(string[] words)
+        {
+            if (this.State == States.ReadWrite)
+            {
+                foreach (string word in words)
+                {
+                    var index = this.Words.IndexOf(word);
+
+                    if (index > -1)
+                    {
+                        this.Words.RemoveAt(index);
+                        this.Words.Sort();
+                    }
+                    else
+                    {
+                        throw new Exception($"{word} was not found in this instance of Webster.");
+                    }
+                }
+            }
+
+        }
+
+        // Delete list of words
+        public void Delete(List<string> words)
+        {
+            if (this.State == States.ReadWrite)
+            {
+                foreach (string word in words)
+                {
+                    var index = this.Words.IndexOf(word);
+
+                    if (index > -1)
+                    {
+                        this.Words.RemoveAt(index);
+                        this.Words.Sort();
+                    }
+                    else
+                    {
+                        throw new Exception($"{word} was not found in this instance of Webster.");
+                    }
+                }
+            }
+
+        }
+
+        // Added "confirm" for an extra level of security
+        public bool DeleteAllWords(bool confirm)
+        {
+            if (this.State == States.ReadWrite)
+            {
+                if (!confirm)
+                {
+                    return false;
+                }
+
+                this.Words.Clear();
+                return true;
+            }
+
+            return false;
+        }
+
+        public int Size()
+        {
+            return this.Words.Count;
+        }
+
+        public void SetState(States state)
+        {
+            this.State = state;
+        }
+    }
+}
